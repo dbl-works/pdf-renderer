5c198dd5270f9d0cff0e2f0c192ca010c3156e45
Marc Qualie <marc@marcqualie.com>
2021-06-17 19:24:08 +0100

    Add iam dev user

---
+++ terraform/iam.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_user" "app-pdf-render-dev" {
+  name = "app-pdf-render-dev"
+
+  tags = {
+    Project = local.project
+    Environment = "development"
+  }
+}
+
+# Allow access to dev bucket
+resource "aws_iam_policy" "s3-pdf-render-dev-usage" {
+  name        = "S3PDFRenderDevUsage"
+  path        = "/"
+  description = "Allow dev app access to dev bucket"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "BucketPermissions",
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket",
+        "s3:ListAllMyBuckets",
+        "s3:GetBucketLocation"
+      ],
+      "Resource": [
+        "arn:aws:s3:::*"
+      ]
+    },
+    {
+      "Sid": "ObjectPermissions",
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectAcl",
+        "s3:GetObjectVersion",
+        "s3:GetObjectVersionAcl",
+        "s3:PutObject",
+        "s3:PutObjectAcl",
+        "s3:PutObjectVersionAcl"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.dev.bucket}",
+        "arn:aws:s3:::${aws_s3_bucket.dev.bucket}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_user_policy_attachment" "s3-pdf-render-dev-usage" {
+  user = aws_iam_user.app-pdf-render-dev.name
+  policy_arn = aws_iam_policy.s3-pdf-render-dev-usage.arn
+}
