ede4d99b406d702ab9ee5eaa56429d55f2c6b3d4
Marc Qualie <marc@marcqualie.com>
2021-06-24 23:05:48 +0100

    Last ones

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response, Application } from 'express'
 import Generator from './utils/generator'
 
-const app: Application = express();
+const app: Application = express()
 app.use(express.json({ limit: '50mb' }))
 app.get('/', (req: Request, res: Response) => res.send(`API Running...${req.query.content}`))
 
-app.post('/', async(req: Request, res: Response) => {
+app.post('/', async (req: Request, res: Response) => {
   const pdfGenerator = new Generator(req.body.content, req.body.filename, req.body.saveFile)
 
   await pdfGenerator.execute()
