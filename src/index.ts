0a23b9585e1190ee04edd4ffa22bb50269827851
Bruno Goncalves <brunogon14@gmail.com>
2021-06-24 23:00:52 +0100

    Fix linting issues.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
-import express from 'express'
-import { Request, Response, Application } from 'express'
-import { Generator } from './utils/generator'
+import express, { Request, Response, Application } from 'express'
+import Generator from './utils/generator'
 
 const app: Application = express();
 app.use(express.json({ limit: '50mb' }))
-app.get("/", (req: Request, res: Response) => {
-  return res.send(`API Running...${req.query.content}`);
-});
+app.get('/', (req: Request, res: Response) => res.send(`API Running...${req.query.content}`))
 
 app.post('/', async(req: Request, res: Response) => {
   const pdfGenerator = new Generator(req.body.content, req.body.filename, req.body.saveFile)
 
-  const result = await pdfGenerator.execute()
+  await pdfGenerator.execute()
   if (pdfGenerator.saveFile) {
     res.json({ filename: pdfGenerator.filename })
   } else {
@@ -20,9 +17,8 @@ app.post('/', async(req: Request, res: Response) => {
 })
 
 app.listen(5000, () => {
-    console.log(
-      ` 📡 Backend server: ` +
-      ` Running in dev mode on port ${5000}`
-    )
-  }
-)
+  console.log(
+    ' 📡 Backend server: '
+    + ` Running in dev mode on port ${5000}`,
+  )
+})
--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,28 +1,30 @@
-import puppeteer from 'puppeteer'
-import { Page, Browser, PDFOptions } from 'puppeteer'
-import { StoreFile } from './storeFile'
+import puppeteer, { Page, Browser, PDFOptions } from 'puppeteer'
+import StoreFile from './storeFile'
 
-export class Generator {
+export default class Generator {
   content: string
+
   filename: string
+
   saveFile: boolean
+
   pdfMarkup: string
 
-  constructor(content: string, filename: string = `${Date.now()}`, saveFile: boolean = true) {
-      this.content = content
-      if (!filename.match(/\.pdf$/)) {
-        filename += '.pdf'
-      }
-      this.filename = filename
-      this.saveFile = saveFile
-      this.pdfMarkup = ''
+  constructor(content: string, filename = `${Date.now()}`, saveFile = true) {
+    this.content = content
+    this.saveFile = saveFile
+    this.pdfMarkup = ''
+    this.filename = filename
+
+    if (!this.filename.match(/\.pdf$/)) {
+      this.filename += '.pdf'
+    }
   }
 
   async execute(): Promise<void> {
-
     const pdfContent: Buffer = await this.generatePDF()
 
-    if (this.saveFile == true) {
+    if (this.saveFile === true) {
       // Storing the file in S3
       StoreFile.store(pdfContent, this.filename)
     } else {
@@ -38,9 +40,9 @@ export class Generator {
       browser = await puppeteer.launch({
         args: [
           '--no-sandbox',
-          '--disable-setuid-sandbox'
+          '--disable-setuid-sandbox',
         ],
-        executablePath: 'google-chrome-stable'
+        executablePath: 'google-chrome-stable',
       })
 
       const page: Page = await browser.newPage()
@@ -49,13 +51,12 @@ export class Generator {
       // Assign the provided content to the page
       await page.setContent(this.content)
 
-      pdfContent = await page['pdf'](defaultOptions)
+      pdfContent = await page.pdf(defaultOptions)
       browser.close()
-
     } catch (e) {
       console.log(`Error generating the PDF: ${e}`)
-    } finally {
-      return new Promise<Buffer>(resolve => { resolve(pdfContent) })
     }
+
+    return new Promise<Buffer>((resolve) => { resolve(pdfContent) })
   }
 }
