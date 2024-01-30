0e991297daffee80b540e8ec717bd775ba8b8af1
Chris <cbock1337@gmail.com>
2024-01-31 12:15:02 +1300

    Improve healthchecks to ensure we can generate a PDF (#218)

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Page, Browser, PDFOptions } from 'puppeteer'
+import puppeteer, { Browser, Page, PDFOptions } from 'puppeteer'
 import StoreFile from './storeFile'
 import Options from '../models/options'
 
@@ -7,9 +7,9 @@ export default class Generator {
 
   options: Options;
 
-  constructor(content: string, options: Options) {
+  constructor(content: string, options?: Options) {
     this.content = content
-    this.options = options
+    this.options = options || new Options()
   }
 
   async execute(): Promise<void> {
@@ -28,11 +28,22 @@ export default class Generator {
     }
   }
 
+  static async healthCheck(): Promise<boolean> {
+    const content = '<h1>PDF Generator is running!</h1>'
+    const generator = new Generator(
+      content,
+    )
+
+    const pdfContent = await generator.generatePDF()
+
+    return !!pdfContent.length
+  }
+
   private async generatePDF(): Promise<Buffer> {
     let pdfContent: Buffer
     let browser: Browser
     const isLocal = process.env.NODE_ENV === 'development'
-    let puppeteerOptions: any
+    let puppeteerOptions: Record<string, string | string[]>
 
     // locally (at least on macOS), puppeteer can detect the correct path automatically,
     // which is probably found at /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
@@ -53,7 +64,6 @@ export default class Generator {
 
     try {
       browser = await puppeteer.launch(puppeteerOptions)
-
       const page: Page = await browser.newPage()
       const defaultOptions: PDFOptions = {
         format: 'a4',
@@ -65,7 +75,8 @@ export default class Generator {
       await page.setContent(this.content)
 
       pdfContent = await page.pdf(defaultOptions)
-      browser.close()
+
+      await browser.close()
     } catch (e) {
       console.log(`Error generating the PDF: ${e}`)
     }
